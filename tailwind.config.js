b0cf4121f13c9b0101497499f660de3baff72982
Willem De Bie <r0955404@student.thomasmore.be>
2024-10-14 09:53:59 +0200

    Additions

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+      colors: {
+        primary: '#FF6347',
+        secondary: '#FFD700',
+        text: '#2F4F4F',
+        background: '#FFF8DC',
+        accent: '#32CD32',
+      },
+  },
+  plugins: [],
+};
