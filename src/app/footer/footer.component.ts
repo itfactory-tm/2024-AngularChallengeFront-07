8625537bb72c05822cb084abb48686ce410e82be
Willem De Bie <debiewillem@outlook.com>
2024-10-22 12:53:21 -0400

    Add privacy and terms

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-  currentYear: number = new Date().getFullYear();
-
-  ngOnInit() { }
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
 }
