caf9a79a1b680ad06cab072bd398153edb961f32
Jorrit Geurts <r0956231@student.thomasmore.be>
2024-11-23 12:55:05 +0100

    info over tickets ingeladen

---
+++ src/app/ticketType.ts
@@ -0,0 +1,5 @@
+export interface TicketType {
+    typeId: number;
+    naam: string;
+    prijs: number;
+}
--- src/app/tickets/tickets.component.ts
+++ src/app/tickets/tickets.component.ts
@@ -3,6 +3,8 @@ import { Ticket } from '../ticket';
 import { TicketService } from '../ticket.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TicketType } from '../ticketType';
+import { TicketTypeService } from '../ticketType.service';
 
 
 
@@ -16,8 +18,12 @@ import { CommonModule } from '@angular/common';
 export class TicketsComponent implements OnInit{
   @Input() ticket!: Ticket;
 
-  constructor() {}
+  ticketType: TicketType | undefined;
 
-  ngOnInit(): void{}
+  constructor(private ticketTypeService: TicketTypeService) {}
+
+  ngOnInit(): void{
+    this.ticketType = this.ticketTypeService.getTicketTypeById(this.ticket.typeId)
+  }
 
 }
