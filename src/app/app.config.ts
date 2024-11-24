c725ca9ae7b3d36d12be0b23ba3e809d4c7a8c0a
SybrenBackx <r0757135@student.thomasmore.be>
2024-11-24 16:19:41 +0100

    added a login button in the header component and basic authO configuration for the project. run: npm install @auth0/auth0-angular in the environment files the AUTH0_DOMAIN still needs to be filled in. I couldn't access the setting on the site.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection  } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { environment } from '../environments/environment';
+
+const domain = environment.AUTH0_DOMAIN;
+const clientId = environment.AUTH0_CLIENT_ID;
 
 export const appConfig: ApplicationConfig = {
-  providers: [[provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()), provideHttpClient()]]
+  providers: [[provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes, withHashLocation()),
+  provideHttpClient(),
+  provideAuth0({
+    domain: domain,
+    clientId: clientId,
+    authorizationParams: {
+      redirect_uri: environment.redirectUri
+    }
+  }),]]
 };
