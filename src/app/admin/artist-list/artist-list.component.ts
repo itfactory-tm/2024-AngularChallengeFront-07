a89c4d0659177e5298dba6519e5011abb408abb1
SybrenBackx <r0757135@student.thomasmore.be>
2024-11-25 11:40:42 +0100

    -added dummy data to show  artists in table -added styling

--- src/app/admin/artist-list/artist-list.component.ts
+++ src/app/admin/artist-list/artist-list.component.ts
@@ -14,6 +14,7 @@ import { AsyncPipe } from '@angular/common';
 })
 export class ArtistListComponent implements OnInit {
   artists$!: Observable<Artist[]>;
+  artists: Artist[] = [];
   errorMessage: string = '';
 
   constructor(private artistService: ArtistService, private router: Router) {
@@ -24,7 +25,7 @@ export class ArtistListComponent implements OnInit {
   }
 
   getArtists() {
-    this.artists$ = this.artistService.getArtistsApi(); //naam van methode vervangen bij connecteren met API
+    this.artists = this.artistService.getArtists(); //naam van methode vervangen bij connecteren met API
   }
 
   add() {
