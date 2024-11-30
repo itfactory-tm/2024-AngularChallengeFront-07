e23fdc9f680a4a51a87899b5ea87e73d9cd8b7d3
Seppe Van Campfort <r0929446@student.thomasmore.be>
2024-11-30 15:23:43 +0100

    started on sponsor crud

---
+++ src/app/admin/sponsor-list/sponsor-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Sponsor } from '../../interfaces/sponsor';
+import { SponsorService } from '../../services/sponsor.service';
+import { Router } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-sponsor-list',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './sponsor-list.component.html',
+  styleUrl: './sponsor-list.component.css',
+})
+export class SponsorListComponent implements OnInit {
+  sponsors$: Observable<Sponsor[]> = new Observable<Sponsor[]>();
+  errorMessage: string = '';
+
+  constructor(private sponsorService: SponsorService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.getSponsors();
+  }
+
+  getSponsors() {
+    this.sponsors$ = this.sponsorService.getSponsors();
+  }
+
+  add() {
+    this.router.navigate(['admin/sponsor/form'], { state: { mode: 'add' } });
+  }
+
+  edit(id: string) {
+    this.router.navigate(['admin/sponsor/form'], {
+      state: { id: id, mode: 'edit' },
+    });
+  }
+
+  delete(id: string) {
+    this.sponsorService.deleteSponsor(id).subscribe({
+      next: (v) => this.getSponsors(),
+      error: (e) => (this.errorMessage = e.message),
+    });
+  }
+}
--- src/app/interfaces/artist.ts
+++ src/app/interfaces/artist.ts
@@ -1,13 +1,11 @@
 export interface Artist {
-    artiestId: string;
-    naam: string;
-    email: string;
-    beschrijving: string;
-    spotifyLink: string;
-    apiCode: string;
-    spotifyPopularity: number | null;
-    spotifyFollowers: number | null;
-    spotifyPhoto: string;
-    genre: string;
-    edities: string[];
+  artistId: string;
+  name: string;
+  mail: string;
+  description: string;
+  spotifyLink: string;
+  apiCode: string;
+  spotifyPhoto: string;
+  genre: string;
+  edities: string[];
 }
