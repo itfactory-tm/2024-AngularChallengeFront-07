e434711d6ab973d9d4ecc1d7abf359ff62a34e21
wlinator <git@wlinator.org>
2024-12-04 05:44:50 -0500

    Test environment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   AUTH0_DOMAIN: 'dev-o6pnv07uc6lnv4mr.us.auth0.com', //nog aanvullen, te vinden op Auth0 site
   AUTH0_CLIENT_ID: 'G5bhYRxwS2RiN8SSByJLuF4cRZxyKODW',
   redirectUri: 'https://fritfest.com',
-  api_url: 'http://localhost:8080/api',
+  api_url: 'http://api.fritfest.com/api',
   home_url: 'https://fritfest.com',
-  AUTH0_AUDIENCE: 'http://localhost:8080',
+  AUTH0_AUDIENCE: 'http://api.fritfest.com',
 };
