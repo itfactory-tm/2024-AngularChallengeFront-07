dd2a1e619c97ddaaad9e80195eefb30114488606
Willem De Bie <debiewillem@outlook.com>
2024-12-04 09:29:09 -0500

    Fix development env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-export const environmentDev = {
+export const environment = {
   production: false,
   AUTH0_DOMAIN: 'dev-o6pnv07uc6lnv4mr.us.auth0.com', //nog aanvullen, te vinden op Auth0 site
   AUTH0_CLIENT_ID: 'G5bhYRxwS2RiN8SSByJLuF4cRZxyKODW',
