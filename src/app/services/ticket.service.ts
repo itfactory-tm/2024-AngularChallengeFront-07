34d759042ed373ef969def14464b6d672fe16c89
Seppe Van Campfort <r0929446@student.thomasmore.be>
2024-12-04 18:33:28 +0100

    Update ticket.service.ts

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { environmentDev } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +15,7 @@ export class TicketService {
 
   private selectedTickets: Ticket[] = [];
 
-  private ApiUrl = `${environmentDev.api_url}/tickets`;
+  private ApiUrl = `${environment.api_url}/tickets`;
 
   constructor(private apiService: ApiService, private httpClient: HttpClient) {}
 
