3b22d573e4abe0dddb178b674b29740dacef245b
Willem De Bie <r0955404@student.thomasmore.be>
2024-12-06 16:53:20 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  AUTH0_DOMAIN: 'dev-o6pnv07uc6lnv4mr.us.auth0.com', //nog aanvullen, te vinden op Auth0 site
+  AUTH0_DOMAIN: 'dev-o6pnv07uc6lnv4mr.us.auth0.com',
   AUTH0_CLIENT_ID: 'G5bhYRxwS2RiN8SSByJLuF4cRZxyKODW',
-  redirectUri: 'http://localhost:4200',
+  redirectUri: 'https://fritfest.com',
   api_url: 'https://fritfestapi20241203213221.azurewebsites.net/api',
-  home_url: 'http://localhost:4200',
+  home_url: 'https://fritfest.com',
   AUTH0_AUDIENCE: 'https://fritfestapi20241203213221.azurewebsites.net',
 };
